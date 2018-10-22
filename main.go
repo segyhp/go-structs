4928759a815f87f63441f496ddd447a2b8aaa17a
Segy Hendro Pratama <segyhendropratama@gmail.com>
2018-10-22 12:53:12 +0700

    declare struct

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"fmt"
+)
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// 1st way
+	// alex := person{"Alex", "Anderson"}
 
+	//2nd way
+	hendro := person{firstName: "Segy", lastName: "Pratama"}
+
+	fmt.Println(hendro)
 }
